3aa97a05fb3ec691ed86b4fdb8d42a5fa30d7093
krispea3 <christian.bracher@gmail.com>
2019-04-24 12:37:38 +0800

    Basic HTTP get and post

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,71 @@
 <template>
   <div class="container">
-
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-md-6 mx-auto mt-3">
+        <div class="form-group">
+          <label for="user.userName">User name</label>
+          <input v-model="user.userName" class="form-control" type="text" id="userName">
+        </div>
+        <div class="form-group">
+          <label for="user.eMail">Email</label>
+          <input v-model="user.eMail" class="form-control" type="email" id="eMail">
+        </div>
+        <button @click="submit" class="btn btn-primary">Submit</button>
+        <hr>
+        <button @click="getData" class="btn btn-primary">Get Data</button>
+        <br><br>
+        <ul class="list-group">
+          <li v-for="u in users" :key=u.eMail class="list-group-item">{{ u.userName }} - {{ u.eMail}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      user: {
+        userName: "",
+        eMail: "",
+      },
+      users: [],
+    }
+  },
+  methods: {
+    submit() {
+      // The empty 1st parameter is the full http address. We defined it globaly, so no need to give it here (see mai.js)
+      this.$http.post("", this.user)
+        .then(response => {
+          console.log(response);
+        }, error => {
+          console.log(error);
+        })
+      console.log(this.user)
+    },
+    getData() {
+      // The empty get parameter is the full http address. We defined it globaly, so no need to give it here (see mai.js)
+      this.$http.get("")
+        .then(response => {
+          return response.json();
+        }, error => {console.log(error)}) 
+        .then(data => {
+          // Returned data is an object. We need to transform it to an array
+          const dataArray = [];
+          for (let key in data) {
+            dataArray.push(data[key])
+          };
+          this.users = dataArray;
+        }, error => {console.log(error)})      
+    },    
+  },
 }
 </script>
 
+}
 <style>
+label {
+  font-weight: bold;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App.vue';
 
+Vue.use(VueResource);
+// Setting global http-address
+Vue.http.options.root = "https://vue-http-add0e.firebaseio.com/data.json";
 new Vue({
   el: '#app',
   render: h => h(App)
